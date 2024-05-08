d6f75ed295271a9dbc6c0511c6b3f75f905af068
herxch <herxch@hotmail.com>
2024-05-08 10:06:59 -0500

    Wording change

--- src/components/layouts/Main/Hero.js
+++ src/components/layouts/Main/Hero.js
@@ -1,15 +1,16 @@
-import styles from './Hero.module.css';
+import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
     <div className={styles.bgContainer}>
       <div className={styles.heroContainer}>
         <div className={styles.heroContent}>
-          <h1 className={styles.heroTitle}>
-            OUR CUSTOMERS' SUCCESS IS OUR SUCCESS
-          </h1>
+          <h1 className={styles.heroTitle}>Professional, Precise, Personal.</h1>
           <p className={styles.heroSubtitle}>
-            We supply high-quality tires with excellent service
+            Tires are not just black and round anymore, sometimes managing them
+            can be complicated, and that is what we are here for, to make
+            everything simple and efficient. We are more than just a tire
+            company.
           </p>
         </div>
       </div>
