7574e3a0805857fff370b996c5095a284c2cd59b
herxch <herxch@hotmail.com>
2024-09-18 10:56:19 -0500

    Remove News section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainPage from "./components/pages/MainPage";
 import AboutPage from "./components/pages/AboutPage";
 import CareersPage from "./components/pages/CareersPage";
-import NewsPage from "./components/pages/NewsPage";
+// import NewsPage from "./components/pages/NewsPage";
 import ContactPage from "./components/pages/ContactPage";
 import CatalogPage from "./components/pages/CatalogPage";
 
@@ -14,8 +14,9 @@ const App = () => {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/catalog" element={<CatalogPage />} />
         <Route path="/careers" element={<CareersPage />} />
-        <Route path="/news" element={<NewsPage />} />
+        {/* <Route path="/news" element={<NewsPage />} /> */}
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
